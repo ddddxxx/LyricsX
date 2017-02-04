9a3106d049de5f7b404f18e1a0bee05ef5657772
Xander Deng <dengxiang2010@gmail.com>
2017-02-04 12:10:19 +0800

    Initial Commit

---
+++ LyricsX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LyricsX
+//
+//  Created by 邓翔 on 2017/2/4.
+//  Copyright © 2017年 ddddxxx. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LyricsX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LyricsX
+//
+//  Created by 邓翔 on 2017/2/4.
+//  Copyright © 2017年 ddddxxx. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
