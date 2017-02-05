67c29aa25642fd564533d411ce591fc58741bc51
Xander Deng <dengxiang2010@gmail.com>
2017-02-05 12:10:05 +0800

    Add: lyrics and background view

--- LyricsX/DesktopLyricsController.swift
+++ LyricsX/DesktopLyricsController.swift
@@ -13,19 +13,47 @@ class DesktopLyricsController: NSWindowController, NSWindowDelegate {
     
     static let shared = DesktopLyricsController()
     
+    var backgroundView: NSView!
+    var textView: NSTextField!
+    
     convenience init() {
         let lyricsWindow = NSWindow(contentRect: NSZeroRect, styleMask: [.borderless, .fullSizeContentView, .texturedBackground], backing: .buffered, defer: false)
-        lyricsWindow.backgroundColor = NSColor(deviceWhite: 0, alpha: 0.5)
+        lyricsWindow.backgroundColor = NSColor.clear
         lyricsWindow.isOpaque = false
         lyricsWindow.hasShadow = false
         lyricsWindow.ignoresMouseEvents = true
         lyricsWindow.level = Int(CGWindowLevelForKey(.floatingWindow))
         lyricsWindow.collectionBehavior = .canJoinAllSpaces
+        lyricsWindow.contentView?.wantsLayer=true
         self.init(window: lyricsWindow)
-        if let window = self.window {
-            let visibleFrame = NSScreen.main()!.visibleFrame
-            window.setFrame(visibleFrame, display: true)
-            window.delegate = self
+        guard let window = self.window else {
+            return
+        }
+        let visibleFrame = NSScreen.main()!.visibleFrame
+        window.setFrame(visibleFrame, display: true)
+        window.delegate = self
+        
+        let superView = window.contentView!
+        
+        backgroundView = NSView()
+        backgroundView.wantsLayer = true
+        backgroundView.layer?.backgroundColor = CGColor(gray: 0, alpha: 0.5)
+        backgroundView.layer?.cornerRadius = 10
+        
+        textView = NSTextField(wrappingLabelWithString: "LyricsX")
+        textView.textColor = NSColor.white
+        textView.font = NSFont.systemFont(ofSize: 28)
+        
+        backgroundView.addSubview(textView)
+        superView.addSubview(backgroundView)
+        
+        textView.snp.makeConstraints() { make in
+            make.edges.equalToSuperview().inset(EdgeInsets(top: 10, left: 20, bottom: 10, right: 20))
+        }
+        
+        backgroundView.snp.makeConstraints() { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().offset(-20)
         }
     }
 }
