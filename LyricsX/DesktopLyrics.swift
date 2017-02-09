341c9ee76b04de665f4a8ce8edaf1406ee0007b9
Xander Deng <dengxiang2010@gmail.com>
2017-02-09 13:05:39 +0800

    Add desktop lyrics animation

--- LyricsX/DesktopLyrics.swift
+++ LyricsX/DesktopLyrics.swift
@@ -14,9 +14,17 @@ class DesktopLyrics {
     var lyricsWindowController: NSWindowController!
     
     var backgroundView: NSView!
-    var textView: NSTextField!
+    var firstLineLrcView: NSTextField!
+    var secondLineLrcView: NSTextField!
+    var waitingLrcView: NSTextField!
+    var reversed = false
+    
+    var firseLine = ""
+    var secondLine = ""
     
     var enabled = UserDefaults.standard.bool(forKey: DesktopLyricsEnabled)
+    
+    let fontSize = 28.0
     
     init() {
         let visibleFrame = NSScreen.main()!.visibleFrame
@@ -37,39 +45,188 @@ class DesktopLyrics {
         backgroundView.layer?.cornerRadius = 10
         window.contentView?.addSubview(backgroundView)
         
-        textView = NSTextField(wrappingLabelWithString: "LyricsX")
-        textView.textColor = .white
-        textView.font = .systemFont(ofSize: 28)
-        textView.alignment = .center
-        backgroundView.addSubview(textView)
+        firstLineLrcView = NSTextField(labelWithString: "")
+        firstLineLrcView.textColor = .white
+        firstLineLrcView.font = .systemFont(ofSize: CGFloat(fontSize))
+        firstLineLrcView.alignment = .center
+        backgroundView.addSubview(firstLineLrcView)
         
-        textView.snp.makeConstraints() { make in
-            make.edges.equalToSuperview().inset(EdgeInsets(top: 10, left: 20, bottom: 10, right: 20))
-        }
+        secondLineLrcView = NSTextField(labelWithString: "")
+        secondLineLrcView.textColor = .white
+        secondLineLrcView.font = .systemFont(ofSize: CGFloat(fontSize))
+        secondLineLrcView.alignment = .center
+        backgroundView.addSubview(secondLineLrcView)
         
-        backgroundView.snp.makeConstraints() { make in
-            make.centerX.equalToSuperview()
-            make.bottom.equalToSuperview().offset(-20)
-        }
+        waitingLrcView = NSTextField(labelWithString: "")
+        waitingLrcView.textColor = .white
+        waitingLrcView.font = .systemFont(ofSize: CGFloat(fontSize))
+        waitingLrcView.alignment = .center
+        backgroundView.addSubview(waitingLrcView)
+        
+        makeConstraints()
+        
+        displayLrc("LyricsX", secondLine: "")
         
         NotificationCenter.default.addObserver(forName: .lyricsShouldDisplay, object: nil, queue: .main) { n in
-            guard self.enabled else {
-                return
-            }
-            
             let lrc = n.userInfo?["lrc"] as? String ?? ""
-            self.backgroundView.isHidden = lrc == ""
-            if let next = n.userInfo?["next"] as? String, next != "" {
-                self.textView.stringValue = lrc + "\n" + next
-            } else {
-                self.textView.stringValue = lrc
-            }
+            let next = n.userInfo?["next"] as? String ?? ""
+            
+            self.displayLrc(lrc, secondLine: next)
         }
         
         NotificationCenter.default.addObserver(forName: UserDefaults.didChangeNotification, object: nil, queue: .main) { n in
             self.enabled = UserDefaults.standard.bool(forKey: DesktopLyricsEnabled)
             self.backgroundView.isHidden = !self.enabled
         }
+    }
+    
+    var normalConstraint: [SnapKit.Constraint] = []
+    var animatedConstraint: [SnapKit.Constraint] = []
+    
+    var firstLineConstraint: [SnapKit.Constraint] = []
+    var secondLineConstraint: [SnapKit.Constraint] = []
+    var waitingLineConstraint: [SnapKit.Constraint] = []
+    
+    func makeConstraints() {
+        firstLineLrcView.snp.makeConstraints() { make in
+            make.centerX.equalToSuperview()
+            
+            make.lastBaseline.equalTo(secondLineLrcView).offset(-fontSize * 1.5)
+            
+            let cons1 = make.left.greaterThanOrEqualToSuperview().offset(20).constraint
+            let cons2 = make.right.lessThanOrEqualToSuperview().offset(-20).constraint
+            
+            let cons3 = make.top.equalToSuperview().offset(10).priority(750).constraint
+            let cons4 = make.bottom.equalToSuperview().offset(-10).priority(500).constraint
+            normalConstraint += [cons1, cons2, cons3, cons4]
+            
+            firstLineConstraint += [cons3]
+        }
+        
+        secondLineLrcView.snp.makeConstraints() { make in
+            make.centerX.equalToSuperview()
+            make.left.greaterThanOrEqualToSuperview().offset(20)
+            make.right.lessThanOrEqualToSuperview().offset(-20)
+            
+            make.lastBaseline.equalTo(waitingLrcView).offset(-fontSize * 1.5)
+            
+            let cons1 = make.top.equalToSuperview().offset(10).priority(500).constraint
+            let cons2 = make.bottom.equalToSuperview().offset(-10).priority(750).constraint
+            normalConstraint += [cons1, cons2]
+            
+            let cons3 = make.top.equalToSuperview().offset(10).priority(750).constraint
+            let cons4 = make.bottom.equalToSuperview().offset(-10).priority(500).constraint
+            animatedConstraint += [cons3, cons4]
+            
+            secondLineConstraint += [cons2, cons3]
+        }
+        
+        waitingLrcView.snp.makeConstraints() { make in
+            make.centerX.equalToSuperview()
+            
+            let cons1 = make.left.greaterThanOrEqualToSuperview().offset(20).constraint
+            let cons2 = make.right.lessThanOrEqualToSuperview().offset(-20).constraint
+            
+            let cons3 = make.top.equalToSuperview().offset(10).priority(500).constraint
+            let cons4 = make.bottom.equalToSuperview().offset(-10).priority(750).constraint
+            animatedConstraint += [cons1, cons2, cons3, cons4]
+            
+            waitingLineConstraint += [cons4]
+        }
+        
+        updateConstraints(animated: false)
+        
+        backgroundView.snp.makeConstraints() { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().offset(-20)
+        }
+    }
+    
+    func updateConstraints(animated: Bool) {
+        if animated {
+            normalConstraint.forEach() { $0.deactivate() }
+            animatedConstraint.forEach() { $0.activate() }
+        } else {
+            normalConstraint.forEach() { $0.activate() }
+            animatedConstraint.forEach() { $0.deactivate() }
+        }
+    }
+    
+    func updateVisibility(animated: Bool) {
+        guard enabled else {
+            backgroundView.isHidden = true
+            return
+        }
+        backgroundView.isHidden = false
+        
+        if animated {
+            firstLineLrcView.alphaValue = 0
+            waitingLrcView.alphaValue = 1
+            
+            guard secondLineLrcView.stringValue != "" else {
+                secondLineLrcView.isHidden = true
+                secondLineConstraint.forEach() { $0.deactivate() }
+                backgroundView.alphaValue = 0
+                return
+            }
+            secondLineLrcView.isHidden = false
+            backgroundView.alphaValue = 1
+            
+            guard waitingLrcView.stringValue != "" else {
+                waitingLrcView.isHidden = true
+                waitingLineConstraint.forEach() { $0.deactivate() }
+                return
+            }
+            waitingLrcView.isHidden = false
+        } else {
+            firstLineLrcView.alphaValue = 1
+            waitingLrcView.alphaValue = 0
+            
+            guard firstLineLrcView.stringValue != "" else {
+                firstLineLrcView.isHidden = true
+                firstLineConstraint.forEach() { $0.deactivate() }
+                backgroundView.alphaValue = 0
+                return
+            }
+            firstLineLrcView.isHidden = false
+            backgroundView.alphaValue = 1
+            
+            guard secondLineLrcView.stringValue != "" else {
+                secondLineLrcView.isHidden = true
+                secondLineConstraint.forEach() { $0.deactivate() }
+                return
+            }
+            secondLineLrcView.isHidden = false
+        }
+    }
+    
+    func displayLrc(_ firstLine: String, secondLine: String) {
+        guard enabled else {
+            return
+        }
+        
+        if self.firseLine == firstLine, self.secondLine == secondLine {
+            return
+        }
+        
+        self.firseLine = firstLine
+        self.secondLine = secondLine
+        
+        NSAnimationContext.runAnimationGroup({ context in
+            context.duration = 0.2
+            context.allowsImplicitAnimation = true
+            self.secondLineLrcView.stringValue = firstLine
+            self.waitingLrcView.stringValue = secondLine
+            self.updateConstraints(animated: true)
+            self.updateVisibility(animated: true)
+            self.backgroundView.layoutSubtreeIfNeeded()
+        }, completionHandler: {
+            self.firstLineLrcView.stringValue = firstLine
+            self.secondLineLrcView.stringValue = secondLine
+            self.updateConstraints(animated: false)
+            self.updateVisibility(animated: false)
+            self.backgroundView.layoutSubtreeIfNeeded()
+        })
     }
     
 }
