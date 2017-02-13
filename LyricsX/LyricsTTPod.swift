b8006fc2c6f0b320eb881404539552ecdea0343f
Xander Deng <dengxiang2010@gmail.com>
2017-02-13 20:25:24 +0800

    Add: Lyrics source TTPod

---
+++ LyricsX/LyricsTTPod.swift
@@ -0,0 +1,28 @@
+//
+//  LyricsTTPod.swift
+//  LyricsX
+//
+//  Created by 邓翔 on 2017/2/13.
+//  Copyright © 2017年 ddddxxx. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class LyricsTTPod: LyricsSource {
+    
+    func fetchLyrics(title: String, artist: String) -> [LXLyrics] {
+        let urlStr = "http://lp.music.ttpod.com/lrc/down?lrcid=&artist=\(artist)&title=\(title)"
+        let convertedURLStr = urlStr.addingPercentEncoding(withAllowedCharacters: .urlFragmentAllowed)!
+        let url = URL(string: convertedURLStr)!
+        
+        guard let data = try? Data(contentsOf: url),
+            let lrcContent = JSON(data)["data"]["lrc"].string,
+            let lrc = LXLyrics(lrcContent)else {
+            return []
+        }
+        
+        return [lrc]
+    }
+    
+}
