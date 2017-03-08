c8a9e2dd0ffd265d120b6c7b021cae045c068e79
Xander Deng <dengxiang2010@gmail.com>
2017-03-08 13:18:59 +0800

    Split long function

--- LyricsX/AppDelegate.swift
+++ LyricsX/AppDelegate.swift
@@ -22,6 +22,25 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var statusBarMenu: NSMenu!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        registerUserDefaults()
+        
+        helper = iTunesHelper()
+        
+        statusItem = NSStatusBar.system().statusItem(withLength: NSSquareStatusItemLength)
+        desktopLyrics = DesktopLyricsController()
+        menuBarLyrics = MenuBarLyrics()
+        
+        desktopLyrics.showWindow(nil)
+        
+        statusItem.button?.image = #imageLiteral(resourceName: "status_bar_icon")
+        statusItem.menu = statusBarMenu
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    func registerUserDefaults() {
         let directFilter = ["作詞", "作词", "作曲", "編曲", "编曲", "収録", "收录", "演唱", "歌手", "歌曲", "制作", "製作", "歌词", "歌詞", "翻譯", "翻译", "插曲", "插入歌", "主题歌", "主題歌", "片頭曲", "片头曲", "片尾曲", "Lrc", "QQ", "アニメ", "LyricsBy", "ComposedBy", "CharacterSong", "InsertSong", "SoundTrack", "www\\.", "\\.com", "\\.net"]
         let colonFilter = ["by", "歌", "唄", "曲", "作", "唱", "詞", "词", "編", "编"]
         let defaultSavingPath = NSSearchPathForDirectoriesInDomains(.musicDirectory, [.userDomainMask], true).first! + "/LyricsX"
@@ -41,23 +60,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             // Filter
             LyricsDirectFilterKey: directFilter as AnyObject,
             LyricsColonFilterKey: colonFilter as AnyObject,
-        ]
+            ]
         UserDefaults.standard.register(defaults: registerDefaults)
-        
-        helper = iTunesHelper()
-        
-        statusItem = NSStatusBar.system().statusItem(withLength: NSSquareStatusItemLength)
-        desktopLyrics = DesktopLyricsController()
-        menuBarLyrics = MenuBarLyrics()
-        
-        desktopLyrics.showWindow(nil)
-        
-        statusItem.button?.image = #imageLiteral(resourceName: "status_bar_icon")
-        statusItem.menu = statusBarMenu
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
     }
 
     @IBAction func lyricsOffsetStepAction(_ sender: Any) {
