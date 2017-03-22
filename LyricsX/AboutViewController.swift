4fe75a8682d9f4247173af3700e36d9c8bfe3413
Xander Deng <dengxiang2010@gmail.com>
2017-03-23 00:03:40 +0800

    add: credits

--- LyricsX/AboutViewController.swift
+++ LyricsX/AboutViewController.swift
@@ -12,13 +12,17 @@ class AboutViewController: NSViewController {
     
     @IBOutlet weak var appName: NSTextField!
     @IBOutlet weak var appVersion: NSTextField!
+    @IBOutlet var creditsTextView: NSTextView!
     
     override func viewDidLoad() {
         let info = Bundle.main.infoDictionary!
         appName.stringValue = info["CFBundleName"] as! String
         let shortVersion = info["CFBundleShortVersionString"] as! String
         let version = info["CFBundleVersion"] as! String
-        appVersion.stringValue = "\(shortVersion)(\(version))"
+        appVersion.stringValue = "Version \(shortVersion)(\(version))"
+        let creditsURL = Bundle.main.url(forResource: "Credits", withExtension: "rtf")!
+        let credits = try! NSAttributedString(url: creditsURL, options: [:], documentAttributes: nil)
+        creditsTextView.textStorage?.setAttributedString(credits)
     }
     
 }
