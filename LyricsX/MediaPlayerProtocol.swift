1b9499c287bbda3398469d3eaa89aaf45ce90fcb
Xander Deng <dengxiang2010@gmail.com>
2017-03-25 22:20:43 +0800

    add: media player protocol

---
+++ LyricsX/MediaPlayerProtocol.swift
@@ -0,0 +1,36 @@
+//
+//  MediaPlayerProtocol.swift
+//  LyricsX
+//
+//  Created by 邓翔 on 2017/3/25.
+//  Copyright © 2017年 ddddxxx. All rights reserved.
+//
+
+import Foundation
+
+enum MediaPlayerState {
+    case stopped
+    case playing
+    case paused
+    case fastForwarding
+    case rewinding
+}
+
+protocol MediaPlayerDelegate: class {
+    func playerStateChanged(state: MediaPlayerState)
+    func currentTrackChanged(track: MediaTrack?)
+    func playerPositionChanged(position: Double)
+}
+
+protocol MediaPlayer {
+    weak var delegate: MediaPlayerDelegate? { get set }
+    var currentTrack: MediaTrack? { get }
+    var playerState: MediaPlayerState { get }
+}
+
+protocol MediaTrack {
+    var id: String { get }
+    var name: String { get }
+    var album: String { get }
+    var artist: String { get }
+}
