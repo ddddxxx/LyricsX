86deee9846ffb4591dca871904ac50077fc35b08
Xander Deng <dengxiang2010@gmail.com>
2017-03-28 13:13:31 +0800

    feat: edit lyrics on HUD close #3

--- LyricsX/LyricsHUDViewController.swift
+++ LyricsX/LyricsHUDViewController.swift
@@ -11,7 +11,9 @@ import EasyPreference
 
 class LyricsHUDViewController: NSViewController {
     
-    @IBOutlet var lyricsTextView: NSTextView!
+    @IBOutlet weak var lyricsTextView: NSTextView!
+    @IBOutlet weak var withTagButton: NSButton!
+    @IBOutlet weak var editButton: NSButton!
     
     var lyrics: Lyrics? {
         didSet {
@@ -19,15 +21,37 @@ class LyricsHUDViewController: NSViewController {
         }
     }
     
+    dynamic var isEditing = false
     var withTag = Preference[DisplayLyricsWithTag]
     
     override func viewDidLoad() {
         lyrics = appDelegate()?.mediaPlayerHelper.currentLyrics
+        editButton.isEnabled = lyrics != nil
         Preference.subscribe(key: DisplayLyricsWithTag) { [weak self] (change) in
             self?.withTag = change.newValue
             self?.updateTextView()
         }
+        lyricsTextView.bind("isEditable", to: self, withKeyPath: "isEditing")
         super.viewDidLoad()
+    }
+    
+    @IBAction func editLyricsAction(_ sender: NSButton) {
+        if isEditing {
+            if let lrcContent = lyricsTextView.string,
+                var lrc = Lyrics(lrcContent) {
+                lrc.metadata = lyrics!.metadata
+                lrc.filtrate()
+                appDelegate()?.mediaPlayerHelper.currentLyrics = lrc
+                lrc.saveToLocal()
+            }
+            view.window?.close()
+        } else {
+            withTagButton.state = NSOnState
+            withTagButton.isEnabled = false
+            lyricsTextView.isEditable = true
+            isEditing = true
+            sender.title = "Save"
+        }
     }
     
     func updateTextView() {
