a985faf7f78f2c645ab65a16272ab9118ee2a55d
Xander Deng <dengxiang2010@gmail.com>
2017-04-13 11:57:15 +0800

    fix: searching qq lyrics block main thread

--- LyricsX/Lyrics/LyricsQQ.swift
+++ LyricsX/Lyrics/LyricsQQ.swift
@@ -18,34 +18,44 @@ class LyricsQQ: LyricsSource {
     }
     
     func fetchLyrics(title: String, artist: String, completionBlock: @escaping (Lyrics) -> Void) {
+        queue.addOperation {
+            let qqIDs = self.searchQQIDFor(title: title, artist: artist)
+            for (index, qqID) in qqIDs.enumerated() {
+                self.queue.addOperation {
+                    guard var lrc = self.lyricsFor(id: qqID) else {
+                        return
+                    }
+                    
+                    var metadata: [Lyrics.MetadataKey: Any] = [:]
+                    metadata[.source]       = "QQMusic"
+                    metadata[.searchTitle]  = title
+                    metadata[.searchArtist] = artist
+                    metadata[.searchIndex]  = index
+                    metadata[.artworkURL] = URL(string: "http://imgcache.qq.com/music/photo/album/\(qqID%100)/\(qqID).jpg")
+                    
+                    lrc.metadata = metadata
+                    
+                    completionBlock(lrc)
+                }
+            }
+        }
+    }
+    
+    private func searchQQIDFor(title: String, artist: String) -> [Int] {
         let urlString: String = "http://s.music.qq.com/fcgi-bin/music_search_new_platform?t=0&n=10&aggr=1&cr=1&loginUin=0&format=json&inCharset=GB2312&outCharset=utf-8&notice=0&platform=jqminiframe.json&needNewCode=0&p=1&catZhida=0&remoteplace=sizer.newclient.next_song&w=\(title) \(artist)"
         let convertedURLStr = urlString.addingPercentEncoding(withAllowedCharacters: .urlFragmentAllowed)!
         let url = URL(string: convertedURLStr)!
         
         guard let data = try? Data(contentsOf: url), let array = JSON(data)["data"]["song"]["list"].array else {
-            return
+            return []
         }
         
-        for (index, item) in array.enumerated() {
-            queue.addOperation {
-                guard let f = item["f"].string,
-                    let range = f.range(of: "|"),
-                    let id = Int(f.substring(to: range.lowerBound)),
-                    var lrc = self.lyricsFor(id: id) else {
-                    return
-                }
-                
-                var metadata: [Lyrics.MetadataKey: Any] = [:]
-                metadata[.source]       = "QQMusic"
-                metadata[.searchTitle]  = title
-                metadata[.searchArtist] = artist
-                metadata[.searchIndex]  = index
-                metadata[.artworkURL] = URL(string: "http://imgcache.qq.com/music/photo/album/\(id%100)/\(id).jpg")
-                
-                lrc.metadata = metadata
-                
-                completionBlock(lrc)
+        return array.flatMap { item in
+            guard let f = item["f"].string,
+                let range = f.range(of: "|") else {
+                return nil
             }
+            return Int(f.substring(to: range.lowerBound))
         }
     }
     
