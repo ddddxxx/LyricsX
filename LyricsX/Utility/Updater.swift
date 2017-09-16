d583f81383e5752cbed9b4fe5b27e4097a7cce76
Xander Deng <dengxiang2010@gmail.com>
2017-09-16 14:32:01 +0800

    remove: SwiftyJSON

--- LyricsX/Utility/Updater.swift
+++ LyricsX/Utility/Updater.swift
@@ -19,23 +19,25 @@
 //
 
 import Cocoa
-import SwiftyJSON
 import Semver
 
+private let gitHubPath = "ddddxxx/LyricsX"
+private let url = URL(string: "https://api.github.com/repos/\(gitHubPath)/releases/latest")!
+
 var remoteVersion: Semver? {
-    let gitHubPath = "ddddxxx/LyricsX"
-    let url = URL(string: "https://api.github.com/repos/\(gitHubPath)/releases/latest")!
-    guard let data = try? Data(contentsOf: url) else { return nil }
-    let json = JSON(data: data)
-    guard var tag = json["tag_name"].string,
-        json["draft"].bool != true,
-        json["prerelease"].bool != true else {
-            return nil
+    do {
+        let data = try Data(contentsOf: url)
+        let response = try JSONDecoder().decode(GitHubResponse.self, from: data)
+        var tag = response.tag_name
+        guard !response.draft, !response.prerelease else { return nil }
+        if tag.hasPrefix("v") {
+            tag.removeFirst()
+        }
+        return try Semver(tag)
+    } catch {
+        log("failed to read remote varsion: \(error)")
+        return nil
     }
-    if tag[tag.startIndex] == "v" {
-        tag.remove(at: tag.startIndex)
-    }
-    return try? Semver(tag)
 }
 
 var localVersion: Semver {
@@ -106,3 +108,50 @@ func checkForUpdate(force: Bool = false) {
     }
     
 #endif
+
+struct GitHubResponse: Decodable {
+    let tag_name: String
+    let draft: Bool
+    let prerelease: Bool
+    
+    /*
+    let url: URL
+    let assets_url: URL
+    let upload_url: URL?
+    let html_url: URL
+    let id: Int
+    let target_commitish: String
+    let name: String
+    let author: Author
+    let created_at: Date
+    let published_at: Date
+    let tarball_url: URL?
+    let zipball_url: URL?
+    let body: String
+     */
+    
+    // let assets: [Any]
+    
+    struct Author: Decodable {
+        /*
+        let login: String
+        let id: Int
+        let avatar_url: URL?
+        let gravatar_id: String
+        let url: URL?
+        let html_url: URL?
+        let followers_url: URL?
+        let following_url: URL?
+        let gists_url: URL?
+        let starred_url: URL?
+        let subscriptions_url: URL?
+        let organizations_url: URL?
+        let repos_url: URL?
+        let events_url: URL?
+        let received_events_url: URL?
+        let type: String
+        let site_admin: Bool
+         */
+    }
+}
+
