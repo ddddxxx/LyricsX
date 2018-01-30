c714c22660bf08be6736016b62f8695253148c6a
Xander Deng <dengxiang2010@gmail.com>
2018-01-30 13:00:29 +0800

    update: LyricsProvider

---
+++ LyricsX/LyricsMetaData+Extension.swift
@@ -0,0 +1,34 @@
+//
+//  LyricsMetaData+Extension.swift
+//  LyricsX
+//
+//  Created by 邓翔 on 2018/1/27.
+//  Copyright © 2018年 ddddxxx. All rights reserved.
+//
+
+import Foundation
+import LyricsProvider
+
+extension Lyrics.MetaData.Key {
+    static var localURL = Lyrics.MetaData.Key("localURL")
+    static var title    = Lyrics.MetaData.Key("title")
+    static var artist   = Lyrics.MetaData.Key("artist")
+}
+
+extension Lyrics.MetaData {
+    
+    var localURL: URL? {
+        get { return data[.localURL] as? URL }
+        set { data[.localURL] = newValue }
+    }
+    
+    var title: String? {
+        get { return data[.title] as? String }
+        set { data[.title] = newValue }
+    }
+    
+    var artist: String? {
+        get { return data[.artist] as? String }
+        set { data[.artist] = newValue }
+    }
+}
