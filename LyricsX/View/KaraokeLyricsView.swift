9a08b4fabe80533ad5d11a469015ae9c35aa9cce
Xander Deng <dengxiang2010@gmail.com>
2018-04-30 14:34:17 +0800

    minor changes

--- LyricsX/View/KaraokeLyricsView.swift
+++ LyricsX/View/KaraokeLyricsView.swift
@@ -51,9 +51,8 @@ class KaraokeLyricsView: NSBox {
         contentView = stackView
     }
     
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        contentView = stackView
+    required init?(coder decoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     private func updateFontSize() {
