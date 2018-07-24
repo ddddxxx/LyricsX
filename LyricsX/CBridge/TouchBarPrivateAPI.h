66fd9b059041c0ccfff0420f6e1b261973343600
ddddxxx <dengxiang2010@gmail.com>
2018-07-24 09:45:46 +0800

    license announcement

--- LyricsX/CBridge/TouchBarPrivateAPI.h
+++ LyricsX/CBridge/TouchBarPrivateAPI.h
@@ -1,9 +1,21 @@
 //
 //  TouchBarPrivateAPI.h
-//  LyricsX
 //
-//  Created by noctis on 2018/4/10.
-//  Copyright © 2018年 ddddxxx. All rights reserved.
+//  This file is part of LyricsX
+//  Copyright (C) 2017 Xander Deng - https://github.com/ddddxxx/LyricsX
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 #ifndef TouchBarPrivateAPI_h
--- LyricsX/LyricsMetaData+Extension.swift
+++ LyricsX/LyricsMetaData+Extension.swift
@@ -1,9 +1,21 @@
 //
 //  LyricsMetaData+Extension.swift
-//  LyricsX
 //
-//  Created by 邓翔 on 2018/1/27.
-//  Copyright © 2018年 ddddxxx. All rights reserved.
+//  This file is part of LyricsX
+//  Copyright (C) 2017 Xander Deng - https://github.com/ddddxxx/LyricsX
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 import Foundation
